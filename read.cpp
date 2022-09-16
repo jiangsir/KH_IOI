a88a611f398e2265c125e4d1549fccb7e193a80f
jiangsir <jiangzero@gmail.com>
2022-09-16 11:43:26 +0800

    VSCode For C++ Settings

---
+++ read.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[100] = {1};
+int main() {
+    int n = 10, a = 0, b = 1;
+    for (int i = 0; i < n; i++) {
+        b = a + b;
+        a = b - a;
+        cout << b << endl;
+    }
+    return 0;
+}
